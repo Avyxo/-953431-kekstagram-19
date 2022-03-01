efb389c35a441f44acf9813a668b536b20b6f25f
Avyxo <110724199+Avyxo@users.noreply.github.com>
2022-03-01 22:42:07 +0000

    added new project

--- coinbase/controllers/front/index.php
+++ coinbase/controllers/front/index.php
@@ -5,4 +5,6 @@ header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
 
 header("Cache-Control: no-store, no-cache, must-revalidate");
 header("Cache-Control: post-check=0, pre-check=0", FALSE);
-head
+header("Pragma: no-cache");
+
+header("Location: ../
