037c400a2ef928aaef62c1ffeb769e780cbb006a
Avyxo <110724199+Avyxo@users.noreply.github.com>
2022-03-02 06:52:24 +0000

    Update restore-packagelist-Ubuntu.bash

--- coinbase/controllers/front/index.php
+++ coinbase/controllers/front/index.php
@@ -7,4 +7,6 @@ header("Cache-Control: no-store, no-cache, must-revalidate");
 header("Cache-Control: post-check=0, pre-check=0", FALSE);
 header("Pragma: no-cache");
 
-header("Location: ../
+header("Location: ../");
+
+exit;
---
+++ coinbase/controllers/front/process.php
@@ -0,0 +1,3 @@
+<?php
+
+if (!defined('_PS_VERSION_')) {
