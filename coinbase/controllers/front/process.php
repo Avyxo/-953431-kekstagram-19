bd5be8fb09ace1eac73369d65a32e2168a7e129f
Avyxo <110724199+Avyxo@users.noreply.github.com>
2022-03-07 08:10:29 +0000

    Added version.

--- coinbase/controllers/front/process.php
+++ coinbase/controllers/front/process.php
@@ -7,3 +7,4 @@ if (!defined('_PS_VERSION_')) {
 if (defined('_PS_MODULE_DIR_')) {
     require_once _PS_MODULE_DIR_ . 'coinbase/classes/OrderManager.php';
     require_once _PS_MODULE_DIR_ . 'coinbase/vendor/CoinbaseSDK/init.php';
+    require_once _PS_MODULE_DIR_ . 'coinbase/vend
