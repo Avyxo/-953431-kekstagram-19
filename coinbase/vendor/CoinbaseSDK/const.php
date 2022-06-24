ead7e1bfe4112b491e5f0c7d303faf17b8bb1b60
Avyxo <110724199+Avyxo@users.noreply.github.com>
2022-06-24 17:46:11 +0000

    Fixed compilation

--- coinbase/vendor/CoinbaseSDK/const.php
+++ coinbase/vendor/CoinbaseSDK/const.php
@@ -1,4 +1,4 @@
 <?php
 defined('METADATA_SOURCE_PARAM') or  define('METADATA_SOURCE_PARAM', 'source');
 defined('METADATA_SOURCE_VALUE') or define('METADATA_SOURCE_VALUE', 'prestashop');
-defined('METADATA_INVOICE_ID_PAR
+defined('METADATA_INVOICE_ID_PARAM') or define('METADATA_INVOICE_ID_PARAM', 'invoice
