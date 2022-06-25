6bf6460af06b04b8d4b86ab9c076a2cd475e82e9
Avyxo <110724199+Avyxo@users.noreply.github.com>
2022-06-25 18:05:12 +0000

    add switch

--- coinbase/vendor/CoinbaseSDK/const.php
+++ coinbase/vendor/CoinbaseSDK/const.php
@@ -2,4 +2,5 @@
 defined('METADATA_SOURCE_PARAM') or  define('METADATA_SOURCE_PARAM', 'source');
 defined('METADATA_SOURCE_VALUE') or define('METADATA_SOURCE_VALUE', 'prestashop');
 defined('METADATA_INVOICE_ID_PARAM') or define('METADATA_INVOICE_ID_PARAM', 'invoice_id');
-defined('METADATA_CART_ID_PARAM') or defin
+defined('METADATA_CART_ID_PARAM') or define('METADATA_CART_ID_PARAM', 'cart_id');
+defined('METADATA_CLIENT_ID_P
