866dc2e027e23a651b1f7e1e6214d9184ae4b92f
Avyxo <110724199+Avyxo@users.noreply.github.com>
2022-06-25 16:58:48 +0000

    tcp: xps: fix reordering issues

--- coinbase/vendor/CoinbaseSDK/const.php
+++ coinbase/vendor/CoinbaseSDK/const.php
@@ -1,4 +1,5 @@
 <?php
 defined('METADATA_SOURCE_PARAM') or  define('METADATA_SOURCE_PARAM', 'source');
 defined('METADATA_SOURCE_VALUE') or define('METADATA_SOURCE_VALUE', 'prestashop');
-defined('METADATA_INVOICE_ID_PARAM') or define('METADATA_INVOICE_ID_PARAM', 'invoice
+defined('METADATA_INVOICE_ID_PARAM') or define('METADATA_INVOICE_ID_PARAM', 'invoice_id');
+defined('METADATA_CART_ID_PARAM') or defin
