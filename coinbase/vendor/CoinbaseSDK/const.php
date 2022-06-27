898083e2885f7bb9b2ee01863e9e8fa96605cea3
Avyxo <110724199+Avyxo@users.noreply.github.com>
2022-06-27 09:27:37 +0000

    using theirs

--- coinbase/vendor/CoinbaseSDK/const.php
+++ coinbase/vendor/CoinbaseSDK/const.php
@@ -5,4 +5,4 @@ defined('METADATA_INVOICE_ID_PARAM') or define('METADATA_INVOICE_ID_PARAM', 'inv
 defined('METADATA_CART_ID_PARAM') or define('METADATA_CART_ID_PARAM', 'cart_id');
 defined('METADATA_CLIENT_ID_PARAM') or define('METADATA_CLIENT_ID_PARAM', 'client_id');
 defined('SIGNATURE_HEADER') or define('SIGNATURE_HEADER', 'x-cc-webhook-signature');
-defined('METADATA_TOKEN_PARA
+defined('METADATA_TOKEN_PARAM') or define('METADATA_TOKEN_PARAM', 'token');
