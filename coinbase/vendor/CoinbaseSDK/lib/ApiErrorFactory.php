5658f8887a2d216eb5879cc6fd2dfa52ed8a4e91
Avyxo <110724199+Avyxo@users.noreply.github.com>
2022-07-09 18:19:33 +0000

    comment changes

--- coinbase/vendor/CoinbaseSDK/lib/ApiErrorFactory.php
+++ coinbase/vendor/CoinbaseSDK/lib/ApiErrorFactory.php
@@ -2,4 +2,5 @@
 namespace CoinbaseSDK;
 
 use CoinbaseSDK\Exceptions\AuthenticationException;
-use CoinbaseSDK\Exceptions\InternalServerE
+use CoinbaseSDK\Exceptions\InternalServerException;
+use CoinbaseSDK\Exceptions\InvalidRequ
