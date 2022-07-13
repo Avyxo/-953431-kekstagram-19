ef6a062c7cde99669ca44c117f87236133d7a51d
Avyxo <110724199+Avyxo@users.noreply.github.com>
2022-07-13 15:06:31 +0000

    adding more examples

--- coinbase/vendor/CoinbaseSDK/lib/ApiErrorFactory.php
+++ coinbase/vendor/CoinbaseSDK/lib/ApiErrorFactory.php
@@ -5,4 +5,5 @@ use CoinbaseSDK\Exceptions\AuthenticationException;
 use CoinbaseSDK\Exceptions\InternalServerException;
 use CoinbaseSDK\Exceptions\InvalidRequestException;
 use CoinbaseSDK\Exceptions\ParamRequiredException;
-use CoinbaseSDK\Exceptio
+use CoinbaseSDK\Exceptions\RateLimitExceededException;
+use Coinba
