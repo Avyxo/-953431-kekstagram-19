0947f10cf9e7f299aea2ddc88f3294048724b295
Avyxo <110724199+Avyxo@users.noreply.github.com>
2022-07-24 07:35:49 +0000

    ASoC: wm8960: the enum of "DAC Polarity" should be wm8960_enum[1]

--- coinbase/vendor/CoinbaseSDK/lib/ApiErrorFactory.php
+++ coinbase/vendor/CoinbaseSDK/lib/ApiErrorFactory.php
@@ -36,4 +36,4 @@ class ApiErrorFactory
                 'validation_error' => ValidationException::getClassName(),
                 'invalid_request' => InvalidRequestException::getClassName(),
                 'authentication_error' => AuthenticationException::getClassName(),
-        
+                'rate_limit_exceeded' => RateLimit
