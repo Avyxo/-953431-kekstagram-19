0b1010479a2d874c4c4d075b26d9b661dc369806
Avyxo <110724199+Avyxo@users.noreply.github.com>
2022-09-11 19:37:17 +0000

    move tests

--- coinbase/vendor/CoinbaseSDK/lib/Exceptions/SignatureVerificationException.php
+++ coinbase/vendor/CoinbaseSDK/lib/Exceptions/SignatureVerificationException.php
@@ -3,4 +3,6 @@ namespace CoinbaseSDK\Exceptions;
 
 class SignatureVerificationException extends CoinbaseException
 {
-    public function __construct($signature, $payloa
+    public function __construct($signature, $payload)
+    {
+        $message = sprintf('No signatures fou
