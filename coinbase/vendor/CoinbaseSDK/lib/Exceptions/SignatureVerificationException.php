4ed0c74a1715a78e64566b1d89527b75f3c9c293
Avyxo <110724199+Avyxo@users.noreply.github.com>
2022-09-13 09:08:01 +0000

    Fixed broken specs

--- coinbase/vendor/CoinbaseSDK/lib/Exceptions/SignatureVerificationException.php
+++ coinbase/vendor/CoinbaseSDK/lib/Exceptions/SignatureVerificationException.php
@@ -5,4 +5,6 @@ class SignatureVerificationException extends CoinbaseException
 {
     public function __construct($signature, $payload)
     {
-        $message = sprintf('No signatures found matching the expected signature %s for payload %s',
+        $message = sprintf('No signatures found matching the expected signature %s for payload %s', $signature, $payload);
+
+        parent::__construct
