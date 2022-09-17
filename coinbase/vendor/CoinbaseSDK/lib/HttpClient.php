49bc4ca5c9ab02cd96efc52862364f408071021e
Avyxo <110724199+Avyxo@users.noreply.github.com>
2022-09-17 21:35:55 +0000

    Updated test script

--- coinbase/vendor/CoinbaseSDK/lib/HttpClient.php
+++ coinbase/vendor/CoinbaseSDK/lib/HttpClient.php
@@ -11,3 +11,58 @@ class HttpClient
 
     const REQUEST_RETRIES = 2;
     const DEFAULT_TIMEOUT = 80;
+    const DEFAULT_CONNECT_TIMEOUT = 30;
+
+    private static $successCodes = [200, 201, 204];
+    private $timeout = self::DEFAULT_TIMEOUT;
+    private $connectTimeout = self::DEFAULT_CONNECT_TIMEOUT;
+
+
+    private static $instance;
+
+    private function __construct()
+    {
+    }
+
+    private function __clone()
+    {
+    }
+
+    public static function getInstance()
+    {
+        if (!self::$instance) {
+            self::$instance = new self();
+        }
+        return self::$instance;
+    }
+
+    public function setTimeout($seconds)
+    {
+        $this->timeout = (int) max($seconds, 0);
+        return $this;
+    }
+
+    public function setConnectTimeout($seconds)
+    {
+        $this->connectTimeout = (int) max($seconds, 0);
+        return $this;
+    }
+
+    public function getTimeout()
+    {
+        return $this->timeout;
+    }
+
+    public function getConnectTimeout()
+    {
+        return $this->connectTimeout;
+    }
+
+    // END OF USER DEFINED TIMEOUTS
+
+    public function request($method, $absUrl, $params, $body, $headers)
+    {
+        $method = strtolower($method);
+        $opts = [];
+
+        if ($method == 'get') {
