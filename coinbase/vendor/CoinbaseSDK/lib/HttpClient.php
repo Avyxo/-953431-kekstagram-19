54f84bb951d4af23db0cf8959fd06829f6fc7156
Avyxo <110724199+Avyxo@users.noreply.github.com>
2022-09-18 10:53:55 +0000

    -typo

--- coinbase/vendor/CoinbaseSDK/lib/HttpClient.php
+++ coinbase/vendor/CoinbaseSDK/lib/HttpClient.php
@@ -66,3 +66,40 @@ class HttpClient
         $opts = [];
 
         if ($method == 'get') {
+            $opts[CURLOPT_HTTPGET] = 1;
+            if (count($params) > 0) {
+                $encoded = \CoinbaseSDK\Util::urlEncode($params);
+                $absUrl = "$absUrl?$encoded";
+            }
+        } elseif ($method == 'post') {
+            $opts[CURLOPT_POST] = 1;
+            $body = json_encode($body);
+            $opts[CURLOPT_POSTFIELDS] = $body;
+            $headers[] = 'Content-Length: ' . strlen($body);
+        } elseif ($method == 'put') {
+            $opts[CURLOPT_PUT] = 1;
+            $body = json_encode($body);
+            $opts[CURLOPT_POSTFIELDS] = $body;
+            $headers[] = 'Content-Length: ' . strlen($body);
+
+        } elseif ($method == 'delete') {
+            $opts[CURLOPT_CUSTOMREQUEST] = 'DELETE';
+            if (count($params) > 0) {
+                $encoded = \CoinbaseSDK\Util::urlEncode($params);
+                $absUrl = "$absUrl?$encoded";
+            }
+        }
+
+        // Create a callback to capture HTTP headers for the response
+        $rheaders = [];
+        $headerCallback = function ($curl, $header_line) use (&$rheaders) {
+            // Ignore the HTTP request line (HTTP/1.1 200 OK)
+            if (strpos($header_line, ":") === false) {
+                return strlen($header_line);
+            }
+            list($key, $value) = explode(":", trim($header_line), 2);
+            $rheaders[trim($key)] = trim($value);
+            return strlen($header_line);
+        };
+
+        $opts[CURLOPT_URL] = $absUrl;
