6a5f60ebf04972934f4e1d12d78ad714427d2338
Avyxo <110724199+Avyxo@users.noreply.github.com>
2022-10-10 15:39:33 +0000

    Update version for release

--- coinbase/vendor/CoinbaseSDK/lib/Operations/ReadMethodTrait.php
+++ coinbase/vendor/CoinbaseSDK/lib/Operations/ReadMethodTrait.php
@@ -8,4 +8,5 @@ trait ReadMethodTrait
 {
     public static function retrieve($id, $headers = [])
     {
-        if (!\is_sca
+        if (!\is_scalar($id)) {
+            throw new \Exception('Invalid id p
