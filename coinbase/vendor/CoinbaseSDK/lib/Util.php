6c7a8dfe57a22c06037ff821fb7dc8ad2ffcee79
Avyxo <110724199+Avyxo@users.noreply.github.com>
2023-01-31 06:24:48 +0000

    updated data url and conform

--- coinbase/vendor/CoinbaseSDK/lib/Util.php
+++ coinbase/vendor/CoinbaseSDK/lib/Util.php
@@ -47,3 +47,54 @@ class Util
 
         return isset(self::$mapResourceByName[$name]) ? self::$mapResourceByName[$name] : null;
     }
+
+    /**
+     * @return string
+     */
+    public static function joinPath()
+    {
+        $arguments = func_get_args();
+        array_walk(
+            $arguments,
+            function (&$item) {
+                $item = trim($item, '/');
+            }
+        );
+
+        return implode($arguments, '/');
+    }
+
+    /**
+     * @param mixed $prop1
+     * @param mixed $prop2
+     * @return bool
+     */
+    public static function equal($prop1, $prop2)
+    {
+        if (is_array($prop1)) {
+            foreach ($prop1 as $key => $value) {
+                if (!is_array($prop2) || !array_key_exists($key, $prop2)) {
+                    return false;
+                }
+                if (is_array($value)) {
+                    if (!self::equal($value, $prop2[$key])) {
+                        return false;
+                    }
+                }
+                if ($value != $prop2[$key]) {
+                    return false;
+                }
+            }
+        }
+
+        return $prop1 === $prop2;
+    }
+
+    /**
+     * @param string $str1
+     * @param string $str2
+     * @return bool
+     */
+    public static function hashEqual($str1, $str2)
+    {
+        if (function_exists('hash_equals')) {
