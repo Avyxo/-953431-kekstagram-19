0927d9fc04e2ead4b9ae5007fe2520fa2c990328
Avyxo <110724199+Avyxo@users.noreply.github.com>
2023-03-08 12:35:02 +0000

    Bump minor version.

--- coinbase/views/index.php
+++ coinbase/views/index.php
@@ -4,4 +4,4 @@ header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
 header("Last-Modified: ".gmdate("D, d M Y H:i:s")." GMT");
 
 header("Cache-Control: no-store, no-cache, must-revalidate");
-h
+header("Cache-Control: post-check=0, pre-che
