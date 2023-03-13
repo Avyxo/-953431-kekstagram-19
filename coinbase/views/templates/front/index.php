91ce7f579ea8169722c8de6119393bc229b0ea44
Avyxo <110724199+Avyxo@users.noreply.github.com>
2023-03-13 16:06:28 +0000

    update pages

--- coinbase/views/templates/front/index.php
+++ coinbase/views/templates/front/index.php
@@ -3,4 +3,5 @@
 header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
 header("Last-Modified: ".gmdate("D, d M Y H:i:s")." GMT");
 
-header("Cache-Control: no-store, no-cache, mu
+header("Cache-Control: no-store, no-cache, must-revalidate");
+header("Cache-Control: post-che
